23ddd79e7ee46d67a46cc6a44c55e88d11867799
Anton <antonborod11111@gmail.com>
2023-10-02 09:03:53 +0300

    Validation of ISBN

--- BusinessLogic/Validators/BookValidator.cs
+++ BusinessLogic/Validators/BookValidator.cs
@@ -1,4 +1,5 @@
-﻿using BusinessLogic.Models.Book;
+﻿using System.Text.RegularExpressions;
+using BusinessLogic.Models.Book;
 using FluentValidation;
 
 namespace BusinessLogic.Validators;
@@ -13,7 +14,7 @@ public class BookValidator : AbstractValidator<BookDto>
 
         RuleFor(book => book.ISBN13)
             .NotEmpty().WithMessage("ISBN-13 is required.")
-            .Length(13).WithMessage("ISBN-13 must be exactly 13 characters.");
+            .Length(13).WithMessage("ISBN-13 must be exactly 13 characters.").Must(BeAValidIsbn);
 
         RuleFor(book => book.Name)
             .NotEmpty().WithMessage("Name is required.")
@@ -21,6 +22,24 @@ public class BookValidator : AbstractValidator<BookDto>
 
         RuleFor(book => book.Description)
             .MaximumLength(500).WithMessage("Description cannot exceed 500 characters.");
+    }
+    
+    private bool BeAValidIsbn(string isbn)
+    {
+        
+        isbn = isbn.Replace("-", "").Replace(" ", "");
+        if (isbn.Length == 13)
+        {
+            int sum = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                sum += (isbn[i] - '0') * (i % 2 == 0 ? 1 : 3);
+            }
+            int expectedCheckDigit = (10 - (sum % 10)) % 10;
+            return isbn[12] == expectedCheckDigit.ToString()[0];
+        }
+
+        return false;
     }
 }
 
